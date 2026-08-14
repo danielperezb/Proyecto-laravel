11ec81f69a17dfe3f57d432f45a5bfc52d17b6cd
Danielperezbocaney@gmail.com <67967393+danielperezb@users.noreply.github.com>
2026-08-13 20:52:17 -0400

    componentes

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -30,6 +30,6 @@
 
    @endphp
 
-   <div class="p-4 mb-4 text-sm rounded-lg {{ $class }}" role="alert">
+   <div {{ $attributes->merge(['class' => 'p-4 text-sm rounded-lg ' . $class]) }} role="alert">
        <span class="font-medium">{{ $title }}</span> {{ $slot }}
    </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,12 +17,18 @@
     <div class="max-w-4xl mx-auto px-4">
         <h1 class="text-2xl font-bold">Bienvenido a la página principal</h1>
 
-        <x-alert type="dark">
+        <x-alert type="info" class="mb-4">
             <x-slot name="title">
                 titulo de la alerta
             </x-slot>
             contenido variable
         </x-alert>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Quisquam, voluptatum.
+        </p>
+
     </div>
 </body>
 
